2fb17a77c717378946f53bccd6f8133cce5ac76a
orkeilius <63045634+orkeilius@users.noreply.github.com>
2023-12-22 13:20:32 +0100

    ajust text on switch button

--- ui/SwitchButton.py
+++ ui/SwitchButton.py
@@ -9,11 +9,11 @@ class SwitchButton(customtkinter.CTkButton):
     mode = 0
     
     def __init__(self,app):
-        super().__init__(app, text=modes[self.mode], command=self.switch,fg_color="gray28",hover_color="gray52")    
+        super().__init__(app, text=modes[self.mode +1 %1], command=self.switch,fg_color="gray28",hover_color="gray52")    
         self.app = app
     
     def switch(self):
-        self.mode = (self.mode + 1) % len(modes)
-        self.configure(text=modes[self.mode])
+        self.mode = (self.mode + 1) % 2
+        self.configure(text=modes[(self.mode + 1) %2])
         self.app.updateGrid(modes[self.mode])
         
